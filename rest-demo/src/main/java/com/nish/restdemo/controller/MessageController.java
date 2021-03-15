19fe7edf13cf10b9aa7e5208bfc3498683738f40
nnares <nishchay.naresh@gmail.com>
2021-03-15 11:15:44 +0530

    +u rest-demo

---
+++ rest-demo/src/main/java/com/nish/restdemo/controller/MessageController.java
@@ -0,0 +1,61 @@
+package com.nish.restdemo.controller;
+
+/*import com.nish.boot.model.Message;
+import com.nish.boot.service.MessageServiceHardCodedValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;*/
+
+import java.util.List;
+
+//@RestController
+public class MessageController {
+
+/*    @Autowired
+    MessageService messageService;*/
+
+ /*   @Autowired
+    MessageServiceHardCodedValue messageService;
+
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET, produces = {"application/text"})
+    public String getTestResponse() {
+        return "Test Response String";
+    }
+
+    @RequestMapping(value = "/messages", method = RequestMethod.GET, produces = {"application/json", "application/xml"})
+    public List<Message> getAllMessages() {
+        return messageService.getAllMessages();
+    }
+
+    @GetMapping("/messages/{messageId}")
+    public Message getMessageById(@PathVariable("messageId") int id) {
+        return messageService.getMessageById(id);
+    }
+
+    @RequestMapping(value = "/messages", method = RequestMethod.POST)
+    public Message createMessage(@RequestBody Message message) {
+        return messageService.createMessage(message);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/messages/{messageId}")
+    public Message updateMessage(@RequestBody Message message, @PathVariable("messageId") int id) {
+        message.setId(id);
+        return messageService.updateMessage(message);
+    }
+
+//    @RequestMapping(method = RequestMethod.PATCH, value = "/messages/{messageId}")
+    @PatchMapping("/messages/{messageId}")
+    public Message updateMessageText(@RequestBody String messageText, @PathVariable("messageId") int id) {
+        Message message =  messageService.getMessageById(id);
+        message.setMessage(messageText);
+        return messageService.updateMessage(message);
+    }
+
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/messages/{messageId}")
+    public Message deleteMessage(@PathVariable("messageId") int id) {
+        return messageService.deleteMessage(id);
+    }
+*/
+}
+

b6307a5031e63476a869c897051bed12de39d09e
nnares <nishchay.naresh@gmail.com>
2021-03-17 00:57:02 +0530

    +u rest-demo

--- rest-demo/src/main/java/com/nish/restdemo/Application.java
+++ rest-demo/src/main/java/com/nish/restdemo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestDemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestDemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }

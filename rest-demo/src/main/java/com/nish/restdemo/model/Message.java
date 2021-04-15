340b385ff2d40ac238f4bbc536dcd4852d449ecc
nnares <nishchay.naresh@gmail.com>
2021-04-15 10:40:19 +0530

    +u rest-demo

--- rest-demo/src/main/java/com/nish/restdemo/model/Message.java
+++ rest-demo/src/main/java/com/nish/restdemo/model/Message.java
@@ -1,5 +1,7 @@
 package com.nish.restdemo.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -7,63 +9,12 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @XmlRootElement
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class Message {
     @Id
     private int id;
     private String message;
     private String author;
     private String created;
-
-    public Message() {
-    }
-
-    public Message(int id, String message, String author, String created) {
-        this.id = id;
-        this.message = message;
-        this.author = author;
-        this.created = created;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getCreated() {
-        return created;
-    }
-
-    public void setCreated(String created) {
-        this.created = created;
-    }
-
-    @Override
-    public String toString() {
-        return "Message{" +
-                "id=" + id +
-                ", message='" + message + '\'' +
-                ", auther='" + author + '\'' +
-                ", created=" + created +
-                '}';
-    }
 
 }
